6b5c137596def88e05859ad9d967cda906039305
Sara Palomares <sarapalomares@me.com>
2017-03-15 13:08:48 -0700

    Added web socket notes

--- server_demo/server.js
+++ server_demo/server.js
@@ -1,29 +1,39 @@
 // require express
 var express = require("express");
-// path module -- try to figure out where and why we use this
+// path module
 var path = require("path");
+
 // create the express app
 var app = express();
-var bodyParser = require('body-parser');
-// use it!
-app.use(bodyParser.urlencoded({ extended: true }));
+
 // static content
 app.use(express.static(path.join(__dirname, "./static")));
+
 // setting up ejs and our views folder
 app.set('views', path.join(__dirname, './views'));
 app.set('view engine', 'ejs');
+
 // root route to render the index.ejs view
 app.get('/', function(req, res) {
- res.render("index");
+      res.render("index");
 })
-// post route for adding a user
-app.post('/users', function(req, res) {
- console.log("POST DATA", req.body);
- // This is where we would add the user to the database
- // Then redirect to the root route
- res.redirect('/');
-})
-// tell the express app to listen on port 8000
-app.listen(8000, function() {
- console.log("listening on port 8000");
+
+// Selects the port and listens. Note that we're now storing our app.listen within a variable called server. Important!!
+var server = app.listen(8000, function(){
+      console.log("Listening on port 8000: Server Test")
+});
+
+//This line goes AFTER our server listener. Unless we have the server variable, this line will not work.
+var io = require("socket.io").listen(server);
+
+//Whenever a connection event happens, run the following code:
+io.sockets.on("connection", function(socket){
+      console.log("We are using sockets!");
+      console.log(socket.id);
+
+      socket.on("button_clicked", function (data){
+            console.log('Someone clicked a button!  Reason: ' + data.reason);
+
+      socket.emit('server_response', {response: "sockets are the best!"});
+      });
 });
